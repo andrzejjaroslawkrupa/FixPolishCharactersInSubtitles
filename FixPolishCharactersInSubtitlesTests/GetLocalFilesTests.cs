c831d484f34ec511b04d65fc72d958b336bd5fd5
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2022-05-18 22:36:00 +0200

    Added two more tests for GetLocalFiles class

--- FixPolishCharactersInSubtitlesTests/GetLocalFilesTests.cs
+++ FixPolishCharactersInSubtitlesTests/GetLocalFilesTests.cs
@@ -2,6 +2,7 @@ using FixPolishCharactersInSubtitles.FileManagement;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Abstractions;
 using Xunit;
 
@@ -9,11 +10,13 @@ namespace FixPolishCharactersInSubtitlesTests
 {
     public class GetLocalFilesTests
     {
-        Mock<IFileSystem> _fileSystemMock;
+        private readonly Mock<IFileSystem> _fileSystemMock;
+        private readonly GetLocalFilesService _getLocalFilesService;
 
         public GetLocalFilesTests()
         {
             _fileSystemMock = new Mock<IFileSystem>();
+            _getLocalFilesService = new GetLocalFilesService(_fileSystemMock.Object);
         }
 
         [Fact]
@@ -22,9 +25,8 @@ namespace FixPolishCharactersInSubtitlesTests
             _fileSystemMock.Setup(f => f.Path.GetDirectoryName(It.IsAny<string>())).Returns(It.IsAny<string>());
             _fileSystemMock.Setup(f => f.Directory.GetFiles(It.IsAny<string>())).Returns(Array.Empty<string>());
             _fileSystemMock.Setup(f => f.Directory.Exists(It.IsAny<string>())).Returns(true);
-            var getLocalFilesService = new GetLocalFilesService(_fileSystemMock.Object);
 
-            List<string> files = getLocalFilesService.GetLocalFiles();
+            List<string> files = _getLocalFilesService.GetLocalFiles();
 
             Assert.Empty(files);
         }
@@ -36,11 +38,25 @@ namespace FixPolishCharactersInSubtitlesTests
             _fileSystemMock.Setup(f => f.Path.GetDirectoryName(It.IsAny<string>())).Returns(It.IsAny<string>());
             _fileSystemMock.Setup(f => f.Directory.GetFiles(It.IsAny<string>())).Returns(fileName);
             _fileSystemMock.Setup(f => f.Directory.Exists(It.IsAny<string>())).Returns(true);
-            var getLocalFilesService = new GetLocalFilesService(_fileSystemMock.Object);
 
-            List<string> files = getLocalFilesService.GetLocalFiles();
+            List<string> files = _getLocalFilesService.GetLocalFiles();
 
             Assert.Equal(fileName, files);
+        }
+
+        [Fact]
+        public void GetLocalFiles_DierctoryNameNull_NullReferenceExceptionIsThrown()
+        {
+            Assert.Throws<NullReferenceException>(() => _getLocalFilesService.GetLocalFiles());
+        }
+
+        [Fact]
+        public void GetLocalFiles_DierctoryDoesNotExist_NullReferenceExceptionIsThrown()
+        {
+            _fileSystemMock.Setup(f => f.Path.GetDirectoryName(It.IsAny<string>())).Returns(It.IsAny<string>());
+            _fileSystemMock.Setup(f => f.Directory.Exists(It.IsAny<string>())).Returns(false);
+
+            Assert.Throws<DirectoryNotFoundException>(() => _getLocalFilesService.GetLocalFiles());
         }
     }
 }
