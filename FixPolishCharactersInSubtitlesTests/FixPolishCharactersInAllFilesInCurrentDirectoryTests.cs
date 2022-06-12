bf4e2cba8572688e0a46fb335d36e96f79140131
Andrzej Krupa <andrzej.jaroslaw.krupa@gmail.com>
2022-06-12 23:31:33 +0200

    Added new tests for FixPolishCharactersInAllFilesInCurrentDirectory

--- FixPolishCharactersInSubtitlesTests/FixPolishCharactersInAllFilesInCurrentDirectoryTests.cs
+++ FixPolishCharactersInSubtitlesTests/FixPolishCharactersInAllFilesInCurrentDirectoryTests.cs
@@ -28,17 +28,57 @@ namespace FixPolishCharactersInSubtitlesTests
         [Fact]
         public void ConvertAllFiles_ValidListOfFiles_AllFilesAreConverted()
         {
-            _getLocalFilesMock.Setup(g => g.GetLocalFiles()).Returns(new List<string> { "file1.srt", "file2.srt" });
-            _fileSystemMock.Setup(f => f.File.ReadAllText(It.IsAny<string>())).Returns(string.Empty);
-            _fileSystemMock.Setup(f => f.File.WriteAllText(It.IsAny<string>(), It.IsAny<string>()));
-            
+            var filesList = new List<string> { "file1.srt", "file2.srt" };
+            SetupSut(filesList);
+
             _fixPolishCharactersInAllFilesInCurrentDirectory.ConvertAllFiles();
 
-            _fileSystemMock.Verify(f => f.File.ReadAllText("file1.srt", Encoding.GetEncoding(1252)), Times.Once);
-            _fileSystemMock.Verify(f => f.File.ReadAllText("file2.srt", Encoding.GetEncoding(1252)), Times.Once);
-            _translateCharactersServiceMock.Verify(t => t.Translate(It.IsAny<string>()), Times.Exactly(2));
-            _fileSystemMock.Verify(f => f.File.WriteAllText("file1.srt", It.IsAny<string>(), Encoding.UTF8), Times.Once);
-            _fileSystemMock.Verify(f => f.File.WriteAllText("file2.srt", It.IsAny<string>(), Encoding.UTF8), Times.Once);
+            AssertFilesWereConverted(filesList);
+        }
+
+        [Fact]
+        public void ConvertAllFiles_NoFiles_NoConversion()
+        {
+            SetupSut(new List<string>());
+
+            _fixPolishCharactersInAllFilesInCurrentDirectory.ConvertAllFiles();
+
+            AssertNoFilesConverted();
+        }
+
+        [Fact]
+        public void ConvertAllFiles_FileWithUnsupportedExtentsion_NoConversion()
+        {
+            SetupSut(new List<string> { "file.unsupported" });
+
+            _fixPolishCharactersInAllFilesInCurrentDirectory.ConvertAllFiles();
+
+            AssertNoFilesConverted();
+        }
+
+        private void SetupSut(List<string> filesList)
+        {
+            _getLocalFilesMock.Setup(g => g.GetLocalFiles()).Returns(filesList);
+            _fileSystemMock.Setup(f => f.File.ReadAllText(It.IsAny<string>())).Returns(string.Empty);
+            _fileSystemMock.Setup(f => f.File.WriteAllText(It.IsAny<string>(), It.IsAny<string>()));
+        }
+
+        private void AssertFilesWereConverted(List<string> files)
+        {
+            foreach (var file in files)
+            {
+                _fileSystemMock.Verify(f => f.File.ReadAllText(file, Encoding.GetEncoding(1252)), Times.Once);
+                _fileSystemMock.Verify(f => f.File.WriteAllText(file, It.IsAny<string>(), Encoding.UTF8), Times.Once);
+            }
+            _translateCharactersServiceMock.Verify(t => t.Translate(It.IsAny<string>()), Times.Exactly(files.Count));
+        }
+
+        private void AssertNoFilesConverted()
+        {
+            _fileSystemMock.Verify(f => f.File.ReadAllText(It.IsAny<string>(), It.IsAny<Encoding>()), Times.Never);
+            _translateCharactersServiceMock.Verify(t => t.Translate(It.IsAny<string>()), Times.Never);
+            _fileSystemMock.Verify(f => f.File.WriteAllText(It.IsAny<string>(), It.IsAny<string>(), Encoding.UTF8), Times.Never);
+            _fileSystemMock.Verify(f => f.File.WriteAllText(It.IsAny<string>(), It.IsAny<string>(), Encoding.UTF8), Times.Never);
         }
     }
 }
